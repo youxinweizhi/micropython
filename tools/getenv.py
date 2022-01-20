47f75750f58246e9cb74600d02bd25455383034d
youxinweizhi <hanxiaomeng@163.com>
2022-01-20 17:51:15 +0800

    add send mail

--- tools/getenv.py
+++ tools/getenv.py
@@ -2,5 +2,7 @@ import os
 MAILPWD = os.environ.get('MAILPWD')
 RECEIVER = os.environ.get('RECEIVER')
 MAILUSER = os.environ.get('MAILUSER')
-print(MAILPWD,RECEIVER,MAILUSER)
+print("*******************************")
 
+print(MAILPWD,RECEIVER,MAILUSER)
+print("*********************************")
